74289ac0991cef688af37203b73bf517b6d3fad8
Hafiz Ahsan <hafiz@idwise.com>
2022-04-18 01:01:43 +0500

    -- module header file cleaned up

--- ios/IDWiseModule.h
+++ ios/IDWiseModule.h
@@ -5,17 +5,10 @@
 //  Created by Hafiz Ahsan on 14/04/2022.
 //
 
-//
-//  YourModule-Bridging-Header.h
-//
-
-#ifndef IDWiseSDKBridge_Bridging_Header_h
-#define IDWiseSDKBridge_Bridging_Header_h
-
 #if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
 #else
 #import <React/RCTBridgeModule.h>
 #endif
 
-#endif /* YourModule_Bridging_Header_h */
+#endif
